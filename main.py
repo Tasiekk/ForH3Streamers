f8d25c963196eb28302847c6e98d9c6917d4ee89
Tasiek <37037797+Tasiekk@users.noreply.github.com>
2021-09-10 13:23:24 +0200

    v.0.0.4 - added background
    - added background
    - no more .grid usage
    - using Canvas from now on

--- main.py
+++ main.py
@@ -13,60 +13,84 @@ root.title("For Heroes 3 Streamers")
 
 #App
 
+
+#Background image
+bg = tk.PhotoImage(file="h3background.png")
+bg_canvas = tk.Canvas(root, width=576, height=240)
+bg_canvas.grid(row=0, column=0, rowspan=6, columnspan=4)
+bg_canvas.create_image(0,0,image=bg, anchor='nw')
+
 #Title
-titleLabel=tk.Label(root, text="This is an app for H3 streamers", font="Righteous")
-titleLabel.grid(row=0, column=0, padx=10, pady=10, columnspan=2)
+titleLabel=tk.Label(root, text="This is an app for H3 streamers", font="Righteous", width=28)
+#titleLabel.grid(row=0, column=0, padx=10, pady=10, columnspan=2)
+titleLabel_window = bg_canvas.create_window(25,20, anchor="nw", window=titleLabel)
 
 #Template
-templateLabel=tk.Label(root, text="Template: ")
-templateLabel.grid(row=0, column=2, padx=10, pady=10)
-templateEntry=tk.Entry(root)
-templateEntry.grid(row=0, column=3, padx=10, pady=10)
+templateLabel=tk.Label(root, text="Template: ", width=15)
+#templateLabel.grid(row=0, column=2, padx=10, pady=10)
+templateLabel_window = bg_canvas.create_window(300,23, anchor="nw", window=templateLabel)
+templateEntry=tk.Entry(root, width=23)
+templateEntry_window = bg_canvas.create_window(415,24, anchor="nw", window=templateEntry)
+#templateEntry.grid(row=0, column=3, padx=10, pady=10)
 
 #Red player
-redLabel=tk.Label(root, text="Red player: ")
-redLabel.grid(row=1, column=0, padx=10, pady=10)
-redEntry=tk.Entry(root)
-redEntry.grid(row=1, column=1, padx=10, pady=10)
+redLabel=tk.Label(root, text="Red player: ", width=15)
+redLabel_window = bg_canvas.create_window(25,50, anchor="nw", window=redLabel)
+#redLabel.grid(row=1, column=0, padx=10, pady=10)
+redEntry=tk.Entry(root, width=23)
+redEntry_window = bg_canvas.create_window(140,51, anchor="nw", window=redEntry)
+#redEntry.grid(row=1, column=1, padx=10, pady=10)
 
-redTownLabel=tk.Label(root, text="Picked town: ")
-redTownLabel.grid(row=2, column=0, padx=10, pady=10)
+redTownLabel=tk.Label(root, text="Picked town: ", width=15, height=2)
+#redTownLabel.grid(row=2, column=0, padx=10, pady=10)
+redTownLabel_window = bg_canvas.create_window(25,77, anchor="nw", window=redTownLabel)
 redTown = tk.StringVar() #defining the value chosen from the dropdown
 redTown.set(info.town_list[0])
-redTownDrop=tk.OptionMenu(root, redTown, *info.town_list).grid(row=2, column=1, padx=10, pady=10)
+redTownDrop=tk.OptionMenu(root, redTown, *info.town_list)#.grid(row=2, column=1, padx=10, pady=10)
+redTownDrop.config(width=16)
+redTownDrop_window = bg_canvas.create_window(140,79, anchor="nw", window=redTownDrop)
 
-redMoneyLabel=tk.Label(root, text="Gold: ")
-redMoneyLabel.grid(row=3, column=0, padx=10, pady=10)
-redMoneyEntry=tk.Entry(root)
-redMoneyEntry.grid(row=3, column=1, padx=10, pady=10)
+redMoneyLabel=tk.Label(root, text="Gold: ", width=15)
+redMoneyLabel_window = bg_canvas.create_window(25,119, anchor="nw", window=redMoneyLabel)
+#redMoneyLabel.grid(row=3, column=0, padx=10, pady=10)
+redMoneyEntry=tk.Entry(root, width=23)
+redMoneyEntry_window = bg_canvas.create_window(140,120, anchor="nw", window=redMoneyEntry)
+#redMoneyEntry.grid(row=3, column=1, padx=10, pady=10)
 
 #Blue player
-blueLabel=tk.Label(root, text="Blue player: ")
-blueLabel.grid(row=1, column=2, padx=10, pady=10)
-blueEntry=tk.Entry(root)
-blueEntry.grid(row=1, column=3, padx=10, pady=10)
+blueLabel=tk.Label(root, text="Blue player: ", width=15)
+redLabel_window = bg_canvas.create_window(300,50, anchor="nw", window=blueLabel)
+#blueLabel.grid(row=1, column=2, padx=10, pady=10)
+blueEntry=tk.Entry(root, width=23)
+redEntry_window = bg_canvas.create_window(415,51, anchor="nw", window=blueEntry)
+#blueEntry.grid(row=1, column=3, padx=10, pady=10)
 
-blueTownLabel=tk.Label(root, text="Picked town: ")
-blueTownLabel.grid(row=2, column=2, padx=10, pady=10)
+blueTownLabel=tk.Label(root, text="Picked town: ", width=15, height=2)
+blueTownLabel_window = bg_canvas.create_window(300,77, anchor="nw", window=blueTownLabel)
+#blueTownLabel.grid(row=2, column=2, padx=10, pady=10)
 blueTown = tk.StringVar() #defining the value chosen from the dropdown
 blueTown.set(info.town_list[0])
-blueTownDrop=tk.OptionMenu(root, blueTown, *info.town_list).grid(row=2, column=3, padx=10, pady=10)
+blueTownDrop=tk.OptionMenu(root, blueTown, *info.town_list)#.grid(row=2, column=3, padx=10, pady=10
+blueTownDrop.config(width=16)
+blueTownDrop_window = bg_canvas.create_window(415,79, anchor="nw", window=blueTownDrop)
 
-blueMoneyLabel=tk.Label(root, text="Gold: ")
-blueMoneyLabel.grid(row=3, column=2, padx=10, pady=10)
-blueMoneyEntry=tk.Entry(root)
-blueMoneyEntry.grid(row=3, column=3, padx=10, pady=10)
+blueMoneyLabel=tk.Label(root, text="Gold: ", width=15)
+blueMoneyLabel_window = bg_canvas.create_window(300,119, anchor="nw", window=blueMoneyLabel)
+#blueMoneyLabel.grid(row=3, column=2, padx=10, pady=10)
+blueMoneyEntry=tk.Entry(root, width=23)
+redMoneyEntry_window = bg_canvas.create_window(415,120, anchor="nw", window=blueMoneyEntry)
+#blueMoneyEntry.grid(row=3, column=3, padx=10, pady=10)
 
 #Submit button
 def submit():
-    submitEntry=tk.Entry(root)
-    submitEntry.grid(row=10,column=0, padx=100, pady=10, ipadx=150, columnspan=4)
+    submitEntry=tk.Entry(root, width=88)
+    submitEntry_window= bg_canvas.create_window(25,205, anchor="nw", window=submitEntry)
     submitEntry.insert(0, templateEntry.get() + " | " + redEntry.get() + " (red, " + redTown.get() + " , " + 
                          redMoneyEntry.get() +")" + " vs " + blueEntry.get() + " (blue, " + blueTown.get() + " , " + 
                          blueMoneyEntry.get() + ")")
     
     
-submitButton=tk.Button(root, text="Submit", command=submit).grid(row=4, column=1, columnspan=2, padx=20, pady=10)
-
+submitButton=tk.Button(root, text="Submit", command=submit, width=20, height=2)#.grid(row=4, column=1, columnspan=2, padx=20, pady=10)
+submitButton_window= bg_canvas.create_window(216,155, anchor="nw", window=submitButton)
 
 root.mainloop()
