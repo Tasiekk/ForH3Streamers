f3316e75a1aaa4cf0cc3e91d36a4480ca3deedd7
Tasiek <37037797+Tasiekk@users.noreply.github.com>
2021-09-10 08:41:57 +0200

    v 0.0.1
    Simple code that shows what this program wants to do

---
+++ info.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Sebastian Uziuk
+Program for Heroes 3 streamers that shows which templaye is played,who plays which castle, 
+how many points they have and what color are they
+"""
+
+town_list = ['Castle','Rampart','Tower','Inferno','Necropolis','Dungeon','Stronghold','Fortress','Conflux','Cove']
+
+print('Which template do you play?')
+template=input()
+print("What is the name of the RED player?")
+red = input()
+print("Choose a town")
+red_town = input()
+while red_town not in town_list:
+    print("Such town doesn't exist, try again")
+    red_town = input()
+print("How much money did RED pay?")
+red_money = input()
+while type(red_money) != int:
+    try:
+        red_money = int(red_money)   
+    except ValueError:
+        print("Add a proper value")
+        red_money = input()
+blue_money = -red_money
+print("What is the name of the BLUE player?")
+blue = input()
+print("Choose a town")
+blue_town = input()
+while blue_town not in town_list:
+    print("Such town doesn't exist, try again")
+    blue_town = input()
+print(template + " |", red ,"(" + red_town + ',', str(red_money) + ")", "vs", 
+      blue ,"(" + blue_town + ',',str(blue_money)+ ")")
+
